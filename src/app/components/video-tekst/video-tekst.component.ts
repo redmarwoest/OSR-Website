ea4f09a0a7813333f7eb6c6ea0926805fba18c34
Redmar Woest <redmarwoest@Redmars-MacBook-Pro.local>
2022-01-27 11:13:00 +0100

    changes

--- src/app/components/video-tekst/video-tekst.component.ts
+++ src/app/components/video-tekst/video-tekst.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 
 @Component({
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-tekst.component.scss']
 })
 export class VideoTekstComponent implements OnInit {
+  @Input() video: any;
+  @Input() title: any;
+  @Input() secondTitle: any;
+  @Input() paragraph: any
+
 
   constructor() { }
 
